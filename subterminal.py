8bc0556e22760b82007205805ab27038bfd0e240
germilla <87410524+Mightyman14386@users.noreply.github.com>
2025-11-23 04:42:36 -0600

    latency added to subterminal

--- subterminal.py
+++ subterminal.py
@@ -1,8 +1,11 @@
 import paho.mqtt.client as mqtt
 import threading
+import time
 
 BROKER_IP = "10.141.41.70"   # HUB IP
 DEVICE_NAME = ""             # laptop2, laptop3
+PACKET_COUNT = 100           # Number of packets per latency check
+counter = 0
 
 # -----------------------------
 # ASK USER WHICH DEVICE THIS IS
@@ -25,9 +28,31 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, msg):
+    global counter
+    t1 = time.time()
     message = msg.payload.decode()
     print(f"\n[{DEVICE_NAME}] Received on {msg.topic}: {message}")
 
+    # Example server usage
+    if message[0] == 'A':
+        num1 = int(message[3:7])
+        num2 = int(message[7:11])
+        sum = num1 + num2
+        time.sleep(0.000001)
+    if message[0] == 'B':
+        num1 = int(message[3:7])
+        num2 = int(message[7:11])
+        diff = num1 - num2
+        time.sleep(0.000001)
+    if message[0] == 'C':
+        num1 = int(message[3:7])
+        num2 = int(message[7:11])
+        product = num1 * num2
+        time.sleep(0.000001)
+
+    if counter == 0:
+        sender_thread(client, t1)
+    counter = (counter + 1) % PACKET_COUNT
     # Save message to a file
     #with open(SAVE_FILE, "a") as f:
      #   f.write(f"{time.ctime()} - {msg.topic}: {message}\n")
@@ -36,19 +61,18 @@ def on_message(client, userdata, msg):
 # -----------------------------
 # SENDER THREAD
 # -----------------------------
-def sender_thread(client):
-    print(f"\n[{DEVICE_NAME}] Type messages to send to HUB (topic: laptop1/inbox)")
-    print("Press Ctrl+C to exit.\n")
+def sender_thread(client, t1):
+    # print(f"\n[{DEVICE_NAME}] Type messages to send to HUB (topic: laptop1/inbox)")
+    # print("Press Ctrl+C to exit.\n")
 
-    while True:
-        try:
-            input_msg = input().strip()
-            # Laptop2 and Laptop3 always send to laptop1
-            client.publish("laptop1/inbox", input_msg)
-            print(f"[{DEVICE_NAME}] Sent to laptop1: {input_msg}")
-        except KeyboardInterrupt:
-            print("Exiting sender thread...")
-            break
+    
+    try:
+        input_msg = time.time() - t1
+        # Laptop2 and Laptop3 always send to laptop1
+        client.publish("laptop1/inbox", input_msg)
+        print(f"[{DEVICE_NAME}] Sent to laptop1: {input_msg}")
+    except KeyboardInterrupt:
+        print("Exiting sender thread...")
 
 # -----------------------------
 # MAIN PROGRAM
@@ -64,4 +88,4 @@ client.connect(BROKER_IP, 1883, 60)
 threading.Thread(target=client.loop_forever, daemon=True).start()
 
 # Run sender in main thread
-sender_thread(client)
+input()
